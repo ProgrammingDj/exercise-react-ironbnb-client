27ec63b410c3e8e2071d2ad744e0f1815fd4abf0
ProgrammingDj <djamelpettes@gmail.com>
2023-06-23 14:29:32 +0200

    Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import ironhackersImg from "./assets/ironhackers.avif"
 import './App.css';
+import {Routes, Route, Navlink} from "react-router-dom"
+import axios from "axios";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome</h1>
-      <img src={ironhackersImg} alt="ironhackers" />
-    </div>
+<div className="App">
+  <Navbar>
+    <Navlink to="/">Home</Navlink>
+    <Navlink to="/">Apartments</Navlink>
+    <Navlink to="/">Details</Navlink>
+    <Navlink to="/">List</Navlink>
+  </Navbar>
+
+  <Routes>
+  <Route path="/" element={<HomePage/>}/>
+  <Route path="/apartments" element={<ApartmentsList/>}/>
+  <Route path="/apartments/:id" element={<ApartmentsDetails/>}/>
+  <Route path="/apartments/create" element={<CreateApartment/>}/>
+  </Routes>
+  
+</div>
   );
 }
 
---
+++ src/components/ApartmentsList.js
@@ -0,0 +1,1 @@
+<h1>This is ApartmentList</h1>
